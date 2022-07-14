7bf22ed741187928f725d9e6a16da895233625cd
ATHULVS2002 <athulvs041@gmail.com>
2022-07-14 21:47:29 +0530

    hello

---
+++ src/app/viewcourse/viewcourse.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-viewcourse',
+  templateUrl: './viewcourse.component.html',
+  styleUrls: ['./viewcourse.component.css']
+})
+export class ViewcourseComponent implements OnInit {
+
+  constructor(private api:ApiService) {api.viewcourse().subscribe((response)=>{this.data=response}) }
+
+  ngOnInit(): void {
+  }
+  data:any=[]
+
+}
